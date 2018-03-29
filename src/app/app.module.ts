2978c7c0fb8c4aa3e717909886d365b37d79aeb9
Kurt Lippert <kurt.lippert@issuerdirect.com>
2018-03-29 10:15:23 -0400

    Added new component and created route for it. Commented out websocket stuff. Added bootstrap / flex html (initial scaffolding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { WebcastModule } from './webcasting/webcast.module';
 import { CompanyModule } from './company/company.module';
 import { DebuggerService } from './common/services/debugger.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { WebcastComponent } from './webcast/webcast.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WebcastComponent,
   ],
   imports: [
     BrowserModule,
