80a16eedbfa7ea9b8e80ce48ecaf85d66c7cf8db
Kurt Lippert <kurt.lippert@issuerdirect.com>
2018-03-30 12:11:44 -0400

    added media conditions to the columns (The large size of 992px seem good enough). Removed some unnecessary margin settings (These were causing overflow making the content slightly off center). Added custom media query (flex-col..-991). We want to change flex direction from row to column at some point. Those custom queries look to be the only way to do it (bootstrap utilities included). Also added material icon stars (just a demo of what it might look like).

--- src/app/webcast/webcast.component.ts
+++ src/app/webcast/webcast.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-webcast',
   templateUrl: './webcast.component.html',
-  styleUrls: ['./webcast.component.css']
+  styleUrls: ['./webcast.component.scss']
 })
 
 export class WebcastComponent implements OnInit {
