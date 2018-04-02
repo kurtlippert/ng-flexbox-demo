70322325609e55b5d5ceea619324ab27ac32c160
Kurt Lippert <kurt.lippert@issuerdirect.com>
2018-04-02 14:18:36 -0400

    Created models. Updated speaker info area. Added text overflow css. Added models with mock data to see how things would fit together. Started on the html for the speaker information. Some fancy css work that truncates text and intelligently adds '...'

--- src/app/webcast/webcast.component.ts
+++ src/app/webcast/webcast.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Company, Speaker } from './webcast.model';
 
 @Component({
   selector: 'app-webcast',
@@ -7,6 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class WebcastComponent implements OnInit {
+
+  company: Company = {
+    id: 0,
+    name: 'Standard Company Name',
+    description: `
+      This is a standard company that does pretty basic things.
+      Standard and boiler-plate things that don't deviate too far from the norm.
+      If you're looking for a company that is "out of the ordinary" or 
+      "wild and crazy" or "decent and respectable", then this certainly would not
+      be the company for you. Far from it. In fact, "respectable" is never a word I
+      would use to describe this standard company. At some point, the text will
+      need to be cut off so I can test the roll-out functionality.
+    `,
+    toggleDescriptionLong: false,
+    website: 'www.google.com',
+    phone: '(555) 555-5555',
+    email: 'test@standardcompany.com',
+    address: '1234 Tester Lane',
+    symbol: 'SCP',
+    marketCap: '1,000,000',
+    fiscalYearEnd: 'never!',
+    sector: 'Food Service',
+    location: 'Tallahasse, Florida',
+  }
+
+  speaker: Speaker = {
+    name: "Me!!",
+    imgUrl: "https://github.com/kurtlippert.png"
+  }
 
   constructor() { }
 
